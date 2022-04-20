1e6337a7d360e9e39273fc039961da53dce28595
SamNyarko <snyar0006@algonquinlive.com>
2022-04-20 06:02:46 -0400

    Finished Website plus added Mock Ups

--- java/main.js
+++ java/main.js
@@ -9,3 +9,17 @@ navOpen.addEventListener("click" , () => {
 navClose.addEventListener("click" , () => {
     navMenu.classList.remove("show-menu");
 });
+
+
+
+
+
+var content = document.getElementsByTagName("body")[0];
+var darkfooter = document.getElementsByTagName("footer")[0];
+var darkMode = document.getElementById("dark-change");
+darkMode.addEventListener("click" , () => {
+    darkMode.classList.toggle("active");
+    content.classList.toggle("night");
+    darkfooter.classList.toggle("nightfooter");
+});
+
